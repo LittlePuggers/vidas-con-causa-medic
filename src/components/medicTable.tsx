24d5231b8cbd67402f2255b75bb95e66afb39255
LittlePuggers <anaim.ecologia@gmail.com>
2024-06-14 15:17:14 -0700

    Added bestUsedBy

--- src/components/medicTable.tsx
+++ src/components/medicTable.tsx
@@ -29,63 +29,72 @@ const rows = [
     concentration: '200mg', 
     name: 'Amoxipet Plus', 
     category: 'Antibiótico', 
-    stock: 35 
+    stock: 35, 
+    bestUsedBy: '31/octubre/2025',
   }, { 
     id: 2, 
     component: 'Tramadol', 
     concentration: '20mg', 
     name: 'Tramadol Pets NRV', 
     category: 'Dolor', 
-    stock: 42 
+    stock: 42, 
+    bestUsedBy: '31/octubre/2025',
   }, { 
     id: 3, 
     component: 'Clindamicina', 
     concentration: '110mg', 
     name: 'Clindacin 10', 
     category: 'Antibiótico', 
-    stock: 45 
+    stock: 45, 
+    bestUsedBy: '31/octubre/2025',
   }, { 
     id: 4, 
     component: 'Metronidazol', 
     concentration: '150mg/ml', 
     name: 'Metronida Pets 150 NRV', 
     category: 'Antibiótico', 
-    stock: 16 
+    stock: 16, 
+    bestUsedBy: '31/octubre/2025',
   }, { 
     id: 5, 
     component: 'Meloxicam', 
     concentration: '1.5mg', 
     name: 'Meloxivet', 
     category: 'Dolor', 
-    stock: null 
+    stock: null, 
+    bestUsedBy: '31/octubre/2025',
   }, { 
     id: 6, 
     component: 'Isoxazolinas: fluralaner', 
     concentration: '250mg', 
     name: 'Bravecto', 
     category: 'Parasiticida', 
-    stock: 150 
+    stock: 150, 
+    bestUsedBy: '31/octubre/2025',
   }, { 
     id: 7, 
     component: 'Isoxazolinas: afoxolaner', 
     concentration: '28.3mg', 
     name: 'NexGard', 
     category: 'Parasiticida', 
-    stock: 44 
+    stock: 44, 
+    bestUsedBy: '31/octubre/2025',
   }, { 
     id: 8, 
     component: 'UC-II', 
     concentration: '10mg', 
     name: 'Flexadin Advanced', 
     category: 'Articulaciones', 
-    stock: 36 
+    stock: 36, 
+    bestUsedBy: '31/octubre/2025',
   }, { 
     id: 9, 
     component: 'Dorzolamida', 
     concentration: '20mg/ml', 
     name: 'Dorzolavet', 
     category: 'Oftálmico', 
-    stock: 65 
+    stock: 65, 
+    bestUsedBy: '31/octubre/2025',
   },
 ];
 
