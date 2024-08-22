58560c9b4ef875f42108a8fee7a824aca84cc91a
LittlePuggers <anaim.ecologia@gmail.com>
2024-08-22 11:03:39 -0700

    Added search bar to medic table

--- front/src/components/MedicTable.tsx
+++ front/src/components/MedicTable.tsx
@@ -4,6 +4,7 @@ import {Product} from '../types/product';
 import {Box, Fab, Typography, styled} from '@mui/material';
 import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
 import componentStyles from './componentStyles';
+import {SearchBar} from './SearchBar.tsx';
 import {useState, useEffect} from 'react';
 import {NewMedicineForm} from './NewMedicineForm';
 import {
@@ -77,90 +78,6 @@ const MedicTable: React.FC<MedicTableProps> = () => {
     },
   ];
 
-  const rows: Product[] = [
-    {
-      id: 1,
-      component: 'Amoxicilina',
-      concentration: '200mg',
-      name: 'Amoxipet Plus',
-      category: 'Antibiótico',
-      stock: 35,
-      bestUsedBy: '31/octubre/2025',
-    },
-    {
-      id: 2,
-      component: 'Tramadol',
-      concentration: '20mg',
-      name: 'Tramadol Pets NRV',
-      category: 'Dolor',
-      stock: 42,
-      bestUsedBy: '31/octubre/2025',
-    },
-    {
-      id: 3,
-      component: 'Clindamicina',
-      concentration: '110mg',
-      name: 'Clindacin 10',
-      category: 'Antibiótico',
-      stock: 45,
-      bestUsedBy: '31/octubre/2025',
-    },
-    {
-      id: 4,
-      component: 'Metronidazol',
-      concentration: '150mg/ml',
-      name: 'Metronida Pets 150 NRV',
-      category: 'Antibiótico',
-      stock: 16,
-      bestUsedBy: '31/octubre/2025',
-    },
-    {
-      id: 5,
-      component: 'Meloxicam',
-      concentration: '1.5mg',
-      name: 'Meloxivet',
-      category: 'Dolor',
-      stock: 0,
-      bestUsedBy: '31/octubre/2025',
-    },
-    {
-      id: 6,
-      component: 'Isoxazolinas: fluralaner',
-      concentration: '250mg',
-      name: 'Bravecto',
-      category: 'Parasiticida',
-      stock: 150,
-      bestUsedBy: '31/octubre/2025',
-    },
-    {
-      id: 7,
-      component: 'Isoxazolinas: afoxolaner',
-      concentration: '28.3mg',
-      name: 'NexGard',
-      category: 'Parasiticida',
-      stock: 44,
-      bestUsedBy: '31/octubre/2025',
-    },
-    {
-      id: 8,
-      component: 'UC-II',
-      concentration: '10mg',
-      name: 'Flexadin Advanced',
-      category: 'Articulaciones',
-      stock: 36,
-      bestUsedBy: '31/octubre/2025',
-    },
-    {
-      id: 9,
-      component: 'Dorzolamida',
-      concentration: '20mg/ml',
-      name: 'Dorzolavet',
-      category: 'Oftálmico',
-      stock: 65,
-      bestUsedBy: '31/octubre/2025',
-    },
-  ];
-
   const [open, setOpen] = useState(false);
 
   const handleClickOpen = () => {
@@ -181,6 +98,9 @@ const MedicTable: React.FC<MedicTableProps> = () => {
         </Fab>
         <NewMedicineForm open={open} handleClose={handleClose} />
       </BoxHead>
+      <Box>
+        <SearchBar></SearchBar>
+      </Box>
       <DataGrid
         rows={medicines}
         columns={columns}
